83752335d976ceedb343f900152ed6e64ef73ff7
Usuario <usuario@example.com>
2025-07-14 12:46:56 -0300

    Limpar arquivos temporários de debug

--- force_click.js
+++
@@ -1,7 +0,0 @@
-// Forçar clique no botão + via JavaScript
-const addBtn = document.querySelector('.add-tab-btn');
-console.log('Botão encontrado:', !!addBtn);
-if (addBtn) {
-    addBtn.click();
-    console.log('Clique executado');
-}
